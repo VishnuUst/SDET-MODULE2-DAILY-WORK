d7f234a62d5efff7d2701b85124962e4e13246c2
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-11-10 17:25:27 +0530

    Add project files.

---
+++ Selenium-examples/AssertionExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+[Serializable]
+internal class AssertionExceptions : Exception
+{
+    public AssertionExceptions()
+    {
+    }
+
+    public AssertionExceptions(string? message) : base(message)
+    {
+    }
+
+    public AssertionExceptions(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected AssertionExceptions(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ Selenium-examples/Program.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V117.WebAuthn;
+
+IWebDriver driver = new ChromeDriver();
+
+driver.Url = "https://www.google.com";
+Thread.Sleep(2000);
+string title = driver.Title;
+try
+{
+    Assert.AreEqual("Google", title);
+    Console.WriteLine("Passed");
+}
+catch(AssertionExceptions)
+{
+    Console.WriteLine("Failed");
+}
+driver.Close();
+
+
+
