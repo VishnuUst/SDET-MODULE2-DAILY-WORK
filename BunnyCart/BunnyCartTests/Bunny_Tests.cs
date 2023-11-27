a190520e52410d51f2be3308b33631fa88b48ced
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-11-27 19:03:56 +0530

    committed

--- BunnyCart/BunnyCartTests/Bunny_Tests.cs
+++ BunnyCart/BunnyCartTests/Bunny_Tests.cs
@@ -1,6 +1,8 @@
 ﻿using BunnyCart.PageObjects;
 using BunnyCart.Utilities;
+using ExcelDataReader;
 using OpenQA.Selenium;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,20 +18,28 @@ namespace BunnyCart.BunnyCartTests
         [Test, Order(1)]
         public void SignUpTest()
         {
+            string? currDir = Directory.GetParent(@"../../../").FullName;
+            string? logfilepath = currDir + "/Log/log_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+            Log.Logger = new LoggerConfiguration().
+                WriteTo.Console().
+                WriteTo.File(logfilepath,rollingInterval: RollingInterval.Day)
+                .CreateLogger();
             BunnyCartHomePage bchp = new(driver);
             bchp.ClickCreateAccount();
             Thread.Sleep(3000);
             try
             {
                 Assert.That(driver?.FindElement(By.XPath("//div[" +
-                    "@class='modal-inner-wrap']//following::h1[2]")).Text,
-                    Is.EqualTo("Create an Account"));
+                    "@class='modal-inner-wrap']//following::h1[2]")).Text=="Create an Account",
+                    $"Test failed for create account");
+                Log.Information("Test passed for create account");
                 test = extent.CreateTest("Create Account Link Test");
                 test.Pass("Create Account Link success");
 
             }
-            catch (AssertionException)
+            catch (AssertionException ex)
             {
+                Log.Error($"Test failed for create account.\n Exception:{ex.Message}");
                 test = extent.CreateTest("Create Account Link Test");
                 test.Fail("Create Account Link failed");
 
@@ -44,9 +54,9 @@ namespace BunnyCart.BunnyCartTests
                 Text, Is.EqualTo("Create an Account"));
                 bh.SignUp("vishnu", "T", "vishnu98@gmail.com", "1234567", "1234567", "9895302409");
                 Thread.Sleep(2000);*/
-            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+            string? currDirs = Directory.GetParent(@"../../../")?.FullName;
 
-            string? excelFilePath = currDir + "/TestData/InputData.xlsx";
+            string? excelFilePath = currDirs + "/TestData/InputData.xlsx";
 
             string? sheetName = "CreateAccount";
 
@@ -85,6 +95,7 @@ namespace BunnyCart.BunnyCartTests
                 bchp.SignUp(firstName, lastName, email, pwd, conpwd, mbno);
 
             }
+            Log.CloseAndFlush();
             //catch(AssertionException)
             //{
             //Console.WriteLine("Sign Up Failed");
