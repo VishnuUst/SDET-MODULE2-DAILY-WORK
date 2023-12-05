53909a1b43816bb5d2de27239d167d11c60be719
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-12-05 10:06:24 +0530

    Hooks Submit

---
+++ BunnyCart_Bdd/Features/SearchAndAddToCart.feature
@@ -0,0 +1,12 @@
+﻿Feature: SearchAndAddToCart
+
+A short summary of the feature
+
+@Search_And_Add_To_Cart
+Scenario: Search for products
+	Given User will be on the home page
+	When User will type the '<searchtext>' in the search box
+	Then Search results are loaded in the same page with '<searchtext>'
+Examples: 
+	| searchtext | 
+	| water      | 
--- BunnyCart_Bdd/StepDefinitions/SearchStepDefinitions.cs
+++ BunnyCart_Bdd/StepDefinitions/SearchStepDefinitions.cs
@@ -7,6 +7,7 @@ using BunnyCart_Bdd;
 using OpenQA.Selenium.DevTools;
 using Serilog;
 using Log = Serilog.Log;
+using BunnyCartBDDTest.Hooks;
 
 namespace BunnyCart_Bdd.StepDefinitions
 {
@@ -14,52 +15,20 @@ namespace BunnyCart_Bdd.StepDefinitions
 
     public class SearchStepDefinitions : Corecodes
     {
-        public static IWebDriver? driver;
-        private IWebElement? passwordInput;
-
-
-        [BeforeFeature]
-        public static void IntializeBrowser()
-        {
-            driver = new ChromeDriver();
-
-
-
-        }
-
-        [AfterFeature]
-        public static void CleanUp()
-        {
-            driver?.Quit();
-        }
-        [BeforeFeature]
-        public static void LogFunctions()
-        {
-            string? currDir = Directory.GetParent(@"../../../").FullName;
-            string? logfilepath = currDir + "/Log/Search_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
-            Log.Logger = new LoggerConfiguration().
-                WriteTo.Console().
-                WriteTo.File(logfilepath, rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-        }
-        [BeforeScenario]
-        public static void NavigateToHomePage()
-        {
-            driver.Navigate().GoToUrl("https://www.bunnycart.com");
-        }
+        
        
         [Given(@"User will be on the Homepage")]
         public void GivenUserWillBeOnTheHomepage()
         {
-            driver.Url = "https://www.bunnycart.com";
-            driver.Manage().Window.Maximize();
+            BeforeHooks.driver.Url = "https://www.bunnycart.com";
+            BeforeHooks. driver.Manage().Window.Maximize();
         }
 
 
         [When(@"User will types the '([^']*)' in the searchbox")]
         public void WhenUserWillTypesTheInTheSearchbox(string searchtext)
         {
-            IWebElement? searchInput = driver.FindElement(By.Id("search"));
+            IWebElement? searchInput =BeforeHooks. driver.FindElement(By.Id("search"));
             searchInput.SendKeys(searchtext);
             searchInput.SendKeys(Keys.Enter);
             Thread.Sleep(3000);
@@ -76,11 +45,11 @@ namespace BunnyCart_Bdd.StepDefinitions
         [Then(@"Search results are loaded in the same page with '([^']*)'")]
         public void ThenSearchResultsAreLoadedInTheSamePageWith(string searchtext)
         {
-            TakeScreenShot(driver);
+            TakeScreenShot(BeforeHooks.driver);
             
             try
             {
-                Assert.That(driver.Url.Contains(searchtext));
+                Assert.That(BeforeHooks.driver.Url.Contains(searchtext));
                 LogTestResult("Search Test", $" {searchtext}Test Pass");
             }
             catch(AssertionException ex)
